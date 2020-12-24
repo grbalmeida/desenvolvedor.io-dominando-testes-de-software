4164e2ff005ff475359b55c4b87f59322d535e41
gilvan <g.r.almeida@live.com>
2020-12-24 16:43:14 -0300

    Utilizando AutoMock

---
+++ TestesDeSoftware/01 - Testes de Unidade/Features.Tests/06 - AutoMock/ClienteServiceAutoMockerTests.cs
@@ -0,0 +1,76 @@
+﻿using Features.Clientes;
+using MediatR;
+using Moq;
+using Moq.AutoMock;
+using System.Linq;
+using System.Threading;
+using Xunit;
+
+namespace Features.Tests
+{
+    [Collection(nameof(ClienteBogusCollection))]
+    public class ClienteServiceAutoMockerTests
+    {
+        private readonly ClienteBogusFixture _clienteBogusFixture;
+
+        public ClienteServiceAutoMockerTests(ClienteBogusFixture clienteBogusFixture)
+        {
+            _clienteBogusFixture = clienteBogusFixture;
+        }
+
+        [Fact(DisplayName = "Adicionar Cliente com Sucesso")]
+        [Trait("Categoria", "Cliente Service AutoMock Tests")]
+        public void ClienteService_Adicionar_DeveExecutarComSucesso()
+        {
+            // Arrange
+            var cliente = _clienteBogusFixture.GerarClienteValido();
+            var mocker = new AutoMocker();
+            var clienteService = mocker.CreateInstance<ClienteService>();
+
+            // Act
+            clienteService.Adicionar(cliente);
+
+            // Assert
+            mocker.GetMock<IClienteRepository>().Verify(r => r.Adicionar(cliente), Times.Once);
+            mocker.GetMock<IMediator>().Verify(m => m.Publish(It.IsAny<INotification>(), CancellationToken.None), Times.Once);
+        }
+
+        [Fact(DisplayName = "Adicionar Cliente com Falha")]
+        [Trait("Categoria", "Cliente Service AutoMock Tests")]
+        public void ClienteService_Adicionar_DeveFalharDevidoClienteInvalido()
+        {
+            // Arrange
+            var cliente = _clienteBogusFixture.GerarClienteInvalido();
+            var mocker = new AutoMocker();
+            var clienteService = mocker.CreateInstance<ClienteService>();
+
+            // Act
+            clienteService.Adicionar(cliente);
+
+            // Assert
+            mocker.GetMock<IClienteRepository>().Verify(r => r.Adicionar(cliente), Times.Never);
+            mocker.GetMock<IMediator>().Verify(m => m.Publish(It.IsAny<INotification>(), CancellationToken.None), Times.Never);
+        }
+
+        [Fact(DisplayName = "Obter Clientes Ativos")]
+        [Trait("Categoria", "Cliente Service AutoMock Tests")]
+        public void ClienteService_ObterTodosAtivos_DeveRetornarApenasClientesAtivos()
+        {
+            // Arrange
+            var mocker = new AutoMocker();
+            var clienteService = mocker.CreateInstance<ClienteService>();
+
+            mocker.GetMock<IClienteRepository>()
+                .Setup(c => c.ObterTodos())
+                .Returns(_clienteBogusFixture.ObterClientesVariados());
+
+            // Act
+            var clientes = clienteService.ObterTodosAtivos();
+
+            // Assert
+            mocker.GetMock<IClienteRepository>().Verify(r => r.ObterTodos(), Times.Once);
+            Assert.True(clientes.Any());
+            Assert.False(clientes.Count(c => !c.Ativo) > 0);
+        }
+    }
+}
---
+++ TestesDeSoftware/01 - Testes de Unidade/Features.Tests/06 - AutoMock/ClienteTestsAutoMockerFixture.cs
@@ -0,0 +1,85 @@
+﻿using Bogus;
+using Bogus.DataSets;
+using Features.Clientes;
+using Moq.AutoMock;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Features.Tests
+{
+    [CollectionDefinition(nameof(ClienteAutoMockerCollection))]
+    public class ClienteAutoMockerCollection : ICollectionFixture<ClienteTestsAutoMockerFixture>
+    {
+
+    }
+
+    public class ClienteTestsAutoMockerFixture : IDisposable
+    {
+        public ClienteService ClienteService;
+        public AutoMocker Mocker;
+
+        public Cliente GerarClienteValido()
+        {
+            return GerarClientes(1, true).FirstOrDefault();
+        }
+
+        public IEnumerable<Cliente> ObterClientesVariados()
+        {
+            var clientes = new List<Cliente>();
+
+            clientes.AddRange(GerarClientes(50, true).ToList());
+            clientes.AddRange(GerarClientes(50, false).ToList());
+
+            return clientes;
+        }
+
+        public IEnumerable<Cliente> GerarClientes(int quantidade, bool ativo)
+        {
+            var genero = new Faker().PickRandom<Name.Gender>();
+
+            var clientes = new Faker<Cliente>()
+                .CustomInstantiator(f => new Cliente(
+                        Guid.NewGuid(),
+                        f.Name.FirstName(),
+                        f.Name.LastName(),
+                        f.Date.Past(80, DateTime.Now.AddYears(-18)),
+                        "",
+                        ativo,
+                        DateTime.Now
+                    ))
+                .RuleFor(c => c.Email, (f, c) => f.Internet.Email(c.Nome.ToLower(), c.Sobrenome.ToLower()));
+
+            return clientes.Generate(quantidade);
+        }
+
+        public Cliente GerarClienteInvalido()
+        {
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "",
+                "",
+                DateTime.Now,
+                "edu2edu.com",
+                true,
+                DateTime.Now
+            );
+
+            return cliente;
+        }
+
+        public ClienteService ObterClienteService()
+        {
+            Mocker = new AutoMocker();
+            ClienteService = Mocker.CreateInstance<ClienteService>();
+
+            return ClienteService;
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
