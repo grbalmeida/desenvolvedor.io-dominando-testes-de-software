5e065bed5eb3e201be4fa0f61ec1a58fe0d45553
gilvan <g.r.almeida@live.com>
2020-12-24 14:27:57 -0300

    Realizando mock de objetos

---
+++ TestesDeSoftware/01 - Testes de Unidade/Features.Tests/05 - Mock/ClienteServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using Features.Clientes;
+using MediatR;
+using Moq;
+using System.Threading;
+using Xunit;
+
+namespace Features.Tests
+{
+    [Collection(nameof(ClienteBogusCollection))]
+    public class ClienteServiceTests
+    {
+        private readonly ClienteBogusFixture _clienteBogusFixture;
+
+        public ClienteServiceTests(ClienteBogusFixture clienteBogusFixture)
+        {
+            _clienteBogusFixture = clienteBogusFixture;
+        }
+
+        [Fact(DisplayName = "Adicionar Cliente com Sucesso")]
+        [Trait("Categoria", "Cliente Service Mock Tests")]
+        public void ClienteService_Adicionar_DeveExecutarComSucesso()
+        {
+            // Arrange
+            var cliente = _clienteBogusFixture.GerarClienteValido();
+            var clienteRepo = new Mock<IClienteRepository>();
+            var mediatr = new Mock<IMediator>();
+
+            var clienteService = new ClienteService(clienteRepo.Object, mediatr.Object);
+
+            // Act
+            clienteService.Adicionar(cliente);
+
+            // Assert
+            clienteRepo.Verify(r => r.Adicionar(cliente), Times.Once);
+            mediatr.Verify(m => m.Publish(It.IsAny<INotification>(), CancellationToken.None), Times.Once);
+        }
+
+        [Fact(DisplayName = "Adicionar Cliente com Falha")]
+        [Trait("Categoria", "Cliente Service Mock Tests")]
+        public void ClienteService_Adicionar_DeveFalharDevidoClienteInvalido()
+        {
+
+        }
+    }
+}
