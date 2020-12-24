86de1e63c8669e98cd2963f92dbf80135564923e
gilvan <g.r.almeida@live.com>
2020-12-24 13:56:50 -0300

    Gerando dados humanos

---
+++ TestesDeSoftware/01 - Testes de Unidade/Features.Tests/04 - Dados Humanos/ClienteBogusFixture.cs
@@ -0,0 +1,56 @@
+﻿using Bogus;
+using Bogus.DataSets;
+using Features.Clientes;
+using System;
+using Xunit;
+
+namespace Features.Tests
+{
+    [CollectionDefinition(nameof(ClienteBogusCollection))]
+    public class ClienteBogusCollection : ICollectionFixture<ClienteBogusFixture>
+    {
+
+    }
+
+    public class ClienteBogusFixture : IDisposable
+    {
+        public Cliente GerarClienteValido()
+        {
+            var genero = new Faker().PickRandom<Name.Gender>();
+
+            var cliente = new Faker<Cliente>()
+                .CustomInstantiator(f => new Cliente(
+                        Guid.NewGuid(),
+                        f.Name.FirstName(),
+                        f.Name.LastName(),
+                        f.Date.Past(80, DateTime.Now.AddYears(-18)),
+                        "",
+                        true,
+                        DateTime.Now
+                    ))
+                .RuleFor(c => c.Email, (f, c) => f.Internet.Email(c.Nome.ToLower(), c.Sobrenome.ToLower()));
+
+            return cliente;
+        }
+
+        public Cliente GerarClienteInvalido()
+        {
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "",
+                "",
+                DateTime.Now,
+                "edu2edu.com",
+                true,
+                DateTime.Now
+            );
+
+            return cliente;
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
---
+++ TestesDeSoftware/01 - Testes de Unidade/Features.Tests/04 - Dados Humanos/ClienteBogusTests.cs
@@ -0,0 +1,45 @@
+﻿using Xunit;
+
+namespace Features.Tests
+{
+    [Collection(nameof(ClienteBogusCollection))]
+    public class ClienteBogusTests
+    {
+        private readonly ClienteBogusFixture _clienteBogusFixture;
+
+        public ClienteBogusTests(ClienteBogusFixture clienteBogusFixture)
+        {
+            _clienteBogusFixture = clienteBogusFixture;
+        }
+
+        [Fact(DisplayName = "Novo Cliente Válido")]
+        [Trait("Categoria", "Cliente Bogus Testes")]
+        public void Cliente_NovoCliente_DeveEstarValido()
+        {
+            // Arrange
+            var cliente = _clienteBogusFixture.GerarClienteValido();
+
+            // Act
+            var result = cliente.EhValido();
+
+            // Assert
+            Assert.True(result);
+            Assert.Equal(0, cliente.ValidationResult.Errors.Count);
+        }
+
+        [Fact(DisplayName = "Novo Cliente Inválido")]
+        [Trait("Categoria", "Cliente Bogus Testes")]
+        public void Cliente_NovoCliente_DeveEstarInvalido()
+        {
+            // Arrange
+            var cliente = _clienteBogusFixture.GerarClienteInvalido();
+
+            // Act
+            var result = cliente.EhValido();
+
+            // Assert
+            Assert.False(result);
+            Assert.NotEqual(0, cliente.ValidationResult.Errors.Count);
+        }
+    }
+}
