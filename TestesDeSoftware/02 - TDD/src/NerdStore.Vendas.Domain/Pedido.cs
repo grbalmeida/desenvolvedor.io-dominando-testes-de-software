4e5d5befd84b7345d042237b76ed7fbf7627ee13
gilvan <g.r.almeida@live.com>
2020-12-27 18:10:02 -0300

    Pedido - Adicionar Item - Parte 1

---
+++ TestesDeSoftware/02 - TDD/src/NerdStore.Vendas.Domain/Pedido.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NerdStore.Vendas.Domain
+{
+    public class Pedido
+    {
+        public decimal ValorTotal { get; private set; }
+        private readonly List<PedidoItem> _pedidoItens;
+        public IReadOnlyCollection<PedidoItem> PedidoItens => _pedidoItens;
+
+        public Pedido()
+        {
+            _pedidoItens = new List<PedidoItem>();
+        }
+
+        public void AdicionarItem(PedidoItem pedidoItem)
+        {
+            _pedidoItens.Add(pedidoItem);
+            ValorTotal = PedidoItens.Sum(i => i.Quantidade * i.ValorUnitario);
+        }
+    }
+
+    public class PedidoItem
+    {
+        public Guid ProdutoId { get; private set; }
+        public string ProdutoNome { get; private set; }
+        public int Quantidade { get; private set; }
+        public decimal ValorUnitario { get; private set; }
+
+        public PedidoItem(Guid produtoId, string produtoNome, int quantidade, decimal valorUnitario)
+        {
+            ProdutoId = produtoId;
+            ProdutoNome = produtoNome;
+            Quantidade = quantidade;
+            ValorUnitario = valorUnitario;
+        }
+    }
+}
---
+++ TestesDeSoftware/02 - TDD/tests/NerdStore.Vendas.Domain.Tests/PedidoTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xunit;
+
+namespace NerdStore.Vendas.Domain.Tests
+{
+    public class PedidoTests
+    {
+        [Fact(DisplayName = "Adicionar Item Novo Pedido")]
+        [Trait("Categoria", "Pedido Tests")]
+        public void AdicionarItemPedido_NovoPedido_DeveAtualizarValor()
+        {
+            // Arrange
+            var pedido = new Pedido();
+            var pedidoItem = new PedidoItem(Guid.NewGuid(), "Produto Teste", 2, 100);
+
+            // Act
+            pedido.AdicionarItem(pedidoItem);
+
+            // Assert
+            Assert.Equal(200, pedido.ValorTotal);
+        }
+    }
+}
