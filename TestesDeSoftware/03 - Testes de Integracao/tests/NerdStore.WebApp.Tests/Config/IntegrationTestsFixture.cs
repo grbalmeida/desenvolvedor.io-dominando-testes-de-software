8bb5988f42eba3398fbe6bd73ebb8d71c1eb9f6d
gilvan <g.r.almeida@live.com>
2020-12-30 21:51:12 -0300

    Configurações necessárias

---
+++ TestesDeSoftware/03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/Config/IntegrationTestsFixture.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using NerdStore.WebApp.MVC;
+using NerdStore.WebApp.Tests.Config;
+using System;
+using System.Net.Http;
+using Xunit;
+
+namespace NerdStore.WebApp.Tests.Config
+{
+    [CollectionDefinition(nameof(IntegrationWebTestsFixtureCollection))]
+    public class IntegrationWebTestsFixtureCollection : ICollectionFixture<IntegrationTestsFixture<StartupWebTests>>
+    {
+
+    }
+
+    [Collection(nameof(IntegrationApiTestsFixtureCollection))]
+    public class IntegrationApiTestsFixtureCollection : ICollectionFixture<IntegrationTestsFixture<StartupApiTests>>
+    {
+
+    }
+
+    public class IntegrationTestsFixture<TStartup> : IDisposable where TStartup : class
+    {
+        public readonly LojaAppFactory<TStartup> Factory;
+        public HttpClient Client;
+
+        public IntegrationTestsFixture()
+        {
+            var clientOptions = new WebApplicationFactoryClientOptions
+            {
+
+            };
+
+            Factory = new LojaAppFactory<TStartup>();
+            Client = Factory.CreateClient(clientOptions);
+        }
+
+        public void Dispose()
+        {
+            Client?.Dispose();
+            Factory?.Dispose();
+        }
+    }
+}
---
+++ TestesDeSoftware/03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/UsuarioTests.cs
@@ -0,0 +1,15 @@
+﻿using NerdStore.WebApp.MVC;
+using NerdStore.WebApp.Tests.Config;
+
+namespace NerdStore.WebApp.Tests
+{
+    public class UsuarioTests
+    {
+        private readonly IntegrationTestsFixture<StartupWebTests> _testsFixture;
+
+        public UsuarioTests(IntegrationTestsFixture<StartupWebTests> testsFixture)
+        {
+            _testsFixture = testsFixture;
+        }
+    }
+}
