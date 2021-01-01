1f304ed91bed2e2d17b16c5c1cefbbfb447f1817
gilvan <g.r.almeida@live.com>
2021-01-01 14:40:19 -0300

    Adicionando testes para o comando RemoverItemPedidoCommand

---
+++ TestesDeSoftware/02 - TDD/tests/NerdStore.Vendas.Application.Tests/Pedidos/RemoverItemPedidoCommandTests.cs
@@ -0,0 +1,41 @@
+﻿using NerdStore.Vendas.Application.Commands;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace NerdStore.Vendas.Application.Tests.Pedidos
+{
+    public class RemoverItemPedidoCommandTests
+    {
+        [Fact(DisplayName = "Remover Item Command Válido")]
+        [Trait("Categoria", "Vendas - Pedido Commands")]
+        public void RemoverItemPedidoCommand_CommandEstaValido_DevePassarNaValidacao()
+        {
+            // Arrange
+            var removerCommand = new RemoverItemPedidoCommand(Guid.NewGuid(), Guid.NewGuid());
+
+            // Act
+            var result = removerCommand.EhValido();
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact(DisplayName = "Remover Item Command Inválido")]
+        [Trait("Categoria", "Vendas - Pedido Commands")]
+        public void RemoverItemPedidoCommand_CommandEstaInvalido_NaoDevePassarNaValidacao()
+        {
+            // Arrange
+            var removerCommand = new RemoverItemPedidoCommand(Guid.Empty, Guid.Empty);
+
+            // Act
+            var result = removerCommand.EhValido();
+
+            // Assert
+            Assert.False(result);
+            Assert.Equal(2, removerCommand.ValidationResult.Errors.Count);
+            Assert.Contains("Id do cliente inválido", removerCommand.ValidationResult.Errors.Select(c => c.ErrorMessage));
+            Assert.Contains("Id do produto inválido", removerCommand.ValidationResult.Errors.Select(c => c.ErrorMessage));
+        }
+    }
+}
