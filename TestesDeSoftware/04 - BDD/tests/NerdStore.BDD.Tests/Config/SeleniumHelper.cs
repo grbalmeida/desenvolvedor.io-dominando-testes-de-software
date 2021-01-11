360e1dc8e22fcbf07d0c7885d38154901664e9fd
gilvan <g.r.almeida@live.com>
2021-01-11 15:32:02 -0300

    Configurando o Selenium da forma certa

--- TestesDeSoftware/04 - BDD/tests/NerdStore.BDD.Tests/Config/SeleniumHelper.cs
+++ TestesDeSoftware/04 - BDD/tests/NerdStore.BDD.Tests/Config/SeleniumHelper.cs
@@ -1,13 +1,61 @@
-﻿namespace NerdStore.BDD.Tests.Config
-{
-    public class SeleniumHelper
-    {
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System;
+using ExpectedConditions = SeleniumExtras.WaitHelpers.ExpectedConditions;
 
+namespace NerdStore.BDD.Tests.Config
+{
+    public class SeleniumHelper : IDisposable
+    {
+        public IWebDriver WebDriver;
+        public readonly ConfigurationHelper Configuration;
+        public WebDriverWait Wait;
+
+        public SeleniumHelper(Browser browser, ConfigurationHelper configuration, bool headless = true)
+        {
+            Configuration = configuration;
+            WebDriver = WebDriverFactory.CreateWebDriver(browser, Configuration.WebDrivers, headless);
+            WebDriver.Manage().Window.Maximize();
+            Wait = new WebDriverWait(WebDriver, TimeSpan.FromSeconds(30));
+        }
+
+        public string ObterUrl()
+        {
+            return WebDriver.Url;
+        }
+
+        public void IrParaUrl(string url)
+        {
+            WebDriver.Navigate().GoToUrl(url);
+        }
+
+        public void ClicarLinkPorTexto(string linkText)
+        {
+            Wait.Until(ExpectedConditions.ElementIsVisible(By.LinkText(linkText)))
+                .Click();
+        }
+
+        public void Dispose()
+        {
+            WebDriver?.Dispose();
+        }
     }
 
     public class ConfigurationHelper
     {
-        
+        private readonly IConfiguration _config;
+
+        public ConfigurationHelper()
+        {
+            _config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
+
+        public string WebDrivers => _config.GetSection("WebDrivers").Value;
     }
 
     public enum Browser
@@ -18,6 +66,31 @@
 
     public static class WebDriverFactory
     {
+        public static IWebDriver CreateWebDriver(Browser browser, string caminhoDriver, bool headless)
+        {
+            IWebDriver webDriver = null;
 
+            switch (browser)
+            {
+                case Browser.Firefox:
+                    var optionsFirefox = new FirefoxOptions();
+                    if (headless)
+                        optionsFirefox.AddArgument("--headless");
+
+                    webDriver = new FirefoxDriver(caminhoDriver, optionsFirefox);
+
+                    break;
+                case Browser.Chrome:
+                    var options = new ChromeOptions();
+                    if (headless)
+                        options.AddArgument("--headless");
+
+                    webDriver = new ChromeDriver(caminhoDriver, options);
+
+                    break;
+            }
+
+            return webDriver;
+        }
     }
 }
