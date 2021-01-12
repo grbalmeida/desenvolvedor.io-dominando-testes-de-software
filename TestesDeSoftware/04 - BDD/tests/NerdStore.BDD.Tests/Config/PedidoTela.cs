f83fccd38a6e0cd20abc31c63e13315b1f02394c
gilvan <g.r.almeida@live.com>
2021-01-12 19:16:27 -0300

    POM - Page Object Model

---
+++ TestesDeSoftware/04 - BDD/tests/NerdStore.BDD.Tests/Config/PedidoTela.cs
@@ -0,0 +1,22 @@
+﻿namespace NerdStore.BDD.Tests.Config
+{
+    public class PedidoTela : PageObjectModel
+    {
+        public PedidoTela(SeleniumHelper helper) : base(helper) { }
+    
+        public void AcessarVitrineDeProdutos()
+        {
+            Helper.IrParaUrl(Helper.Configuration.VitrineUrl);
+        }
+
+        public void ObterDetalhesDoProduto(int posicao = 1)
+        {
+            Helper.ClicarPorXPath($"/html/body/div/main/div/div/div[{posicao}]/span/a");
+        }
+        
+        public bool ValidarProdutoDisponivel()
+        {
+            return Helper.ValidarConteudoUrl(Helper.Configuration.ProdutoUrl);
+        }
+    }
+}
--- TestesDeSoftware/04 - BDD/tests/NerdStore.BDD.Tests/Pedido/Pedido_AdicionarItemAoCarrinhoSteps.cs
+++ TestesDeSoftware/04 - BDD/tests/NerdStore.BDD.Tests/Pedido/Pedido_AdicionarItemAoCarrinhoSteps.cs
@@ -9,23 +9,28 @@ namespace NerdStore.BDD.Tests.Pedido
     public class Pedido_AdicionarItemAoCarrinhoSteps
     {
         private readonly AutomacaoWebTestsFixture _testsFixture;
+        private readonly PedidoTela _pedidoTela;
+
+        private string _urlProduto;
 
         public Pedido_AdicionarItemAoCarrinhoSteps(AutomacaoWebTestsFixture testsFixture)
         {
             _testsFixture = testsFixture;
+            _pedidoTela = new PedidoTela(testsFixture.BrowserHelper);
         }
 
         [Given(@"Que um produto esteja na vitrine")]
         public void DadoQueUmProdutoEstejaNaVitrine()
         {
             // Arrange
-            _testsFixture.BrowserHelper.IrParaUrl("https://desenvolvedor.io");
-            _testsFixture.BrowserHelper.ClicarLinkPorTexto("Entrar");
-            _testsFixture.BrowserHelper.PreencherTextBoxPorId("Email", "contato@teste.com");
+            _pedidoTela.AcessarVitrineDeProdutos();
 
             // Act
+            _pedidoTela.ObterDetalhesDoProduto();
+            _urlProduto = _pedidoTela.ObterUrl();
 
             //Assert
+            Assert.True(_pedidoTela.ValidarProdutoDisponivel());
         }
         
         [Given(@"Esteja disponível no estoque")]
