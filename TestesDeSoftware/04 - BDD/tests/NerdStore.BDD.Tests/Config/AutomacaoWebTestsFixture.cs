8c05fc243eae68cfc7abe48eeb9740113bf8c1b7
gilvan <g.r.almeida@live.com>
2021-01-12 13:01:19 -0300

    Automatizando a criação do WebDriver do Selenium

---
+++ TestesDeSoftware/04 - BDD/tests/NerdStore.BDD.Tests/Config/AutomacaoWebTestsFixture.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace NerdStore.BDD.Tests.Config
+{
+    [CollectionDefinition(nameof(AutomacaoWebTestsFixture))]
+    public class AutomacaoWebFixtureCollection : ICollectionFixture<AutomacaoWebTestsFixture> { }
+
+    public class AutomacaoWebTestsFixture
+    {
+        public SeleniumHelper BrowserHelper;
+        public readonly ConfigurationHelper Configuration;
+
+        public AutomacaoWebTestsFixture()
+        {
+            Configuration = new ConfigurationHelper();
+            BrowserHelper = new SeleniumHelper(Browser.Chrome, Configuration, false);
+        }
+    }
+}
--- TestesDeSoftware/04 - BDD/tests/NerdStore.BDD.Tests/Pedido/Pedido_AdicionarItemAoCarrinhoSteps.cs
+++ TestesDeSoftware/04 - BDD/tests/NerdStore.BDD.Tests/Pedido/Pedido_AdicionarItemAoCarrinhoSteps.cs
@@ -1,20 +1,27 @@
 ﻿using NerdStore.BDD.Tests.Config;
-using OpenQA.Selenium.Chrome;
 using TechTalk.SpecFlow;
+using Xunit;
 
 namespace NerdStore.BDD.Tests.Pedido
 {
     [Binding]
+    [CollectionDefinition(nameof(AutomacaoWebFixtureCollection))]
     public class Pedido_AdicionarItemAoCarrinhoSteps
     {
+        private readonly AutomacaoWebTestsFixture _testsFixture;
+
+        public Pedido_AdicionarItemAoCarrinhoSteps(AutomacaoWebTestsFixture testsFixture)
+        {
+            _testsFixture = testsFixture;
+        }
+
         [Given(@"Que um produto esteja na vitrine")]
         public void DadoQueUmProdutoEstejaNaVitrine()
         {
             // Arrange
-            var browser = new SeleniumHelper(Browser.Chrome, new ConfigurationHelper(), false);
-            browser.IrParaUrl("https://desenvolvedor.io");
-            browser.ClicarLinkPorTexto("Entrar");
-            browser.PreencherTextBoxPorId("Email", "contato@teste.com");
+            _testsFixture.BrowserHelper.IrParaUrl("https://desenvolvedor.io");
+            _testsFixture.BrowserHelper.ClicarLinkPorTexto("Entrar");
+            _testsFixture.BrowserHelper.PreencherTextBoxPorId("Email", "contato@teste.com");
 
             // Act
 
